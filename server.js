a630fb4b44a644c598bca461e791e59c8992d85d
Keith Flynn <flynn.keith@gmail.com>
2023-10-04 17:27:51 -0400

    Fix require dotenv for .env

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/subscribers', { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
-app.listen(3000, () => console.log('Server started...'));
+app.listen(3000, () => console.log('Server started...')); 
