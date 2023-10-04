49503f31c868634f24d72019cc010f03cdc145bb
Keith Flynn <flynn.keith@gmail.com>
2023-10-04 17:15:48 -0400

    Fix critical bug
    It helps if you install mongodb first.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/subscribers', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1/subscribers', { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
 
-app.listen(3000, () => console.log('Server started'));
+app.listen(3000, () => console.log('Server started...'));
